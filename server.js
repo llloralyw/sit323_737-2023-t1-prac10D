990cccd05d920cc9ab57125fea9a0ffc06c4cd4e
Lora <88323668+llloralyw@users.noreply.github.com>
2023-06-04 22:38:49 +1000

    Add files via upload

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ const HOST = '0.0.0.0';
 //App
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello World After Changes');
+    res.send('I made node changes');
 });
 
 app.listen(PORT, HOST, () => {
-    console.log('Runnung on http://${HOST}:{PORT}');
+    console.log('Running on http://${HOST}:${PORT}');
 });
